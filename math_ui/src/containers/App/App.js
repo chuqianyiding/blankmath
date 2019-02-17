28618772e81e660b3dc1402ce52e45b90c070f7f
Yanyan Li <yyrobot@gmail.com>
2019-02-16 17:19:03 -0800

    init checkin

---
+++ math_ui/src/containers/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>hello, world</div>
+    );
+  }
+}
+
+export default App;
