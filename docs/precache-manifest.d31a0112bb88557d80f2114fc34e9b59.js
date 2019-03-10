5855228aee6657af77cab93a923f8ce806420d3c
Travis CI <d@domenic.me>
2019-03-10 23:10:37 +0000

    Deploy to GitHub Pages: 244ebf46c9b83652ed323c45f0cea92cd27f8aa3

---
+++ docs/precache-manifest.d31a0112bb88557d80f2114fc34e9b59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cbb2913074f2cbd1169",
+    "url": "/static/css/main.e3b6265b.chunk.css"
+  },
+  {
+    "revision": "2cbb2913074f2cbd1169",
+    "url": "/static/js/main.2cbb2913.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "298fe66a58c2319ee195",
+    "url": "/static/js/2.298fe66a.chunk.js"
+  },
+  {
+    "revision": "af7060b2577b9f66c0c87f6e331d6185",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2169e38da019a6ba18492d66dd916bc.js"
+  "/precache-manifest.d31a0112bb88557d80f2114fc34e9b59.js"
 );
 
 workbox.clientsClaim();
