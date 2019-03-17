891735289de1314490cb14ccb5a44b2e84d5bd18
Yanyan Li <yyrobot@gmail.com>
2019-03-16 17:13:13 -0700

    fix warning

--- math_ui/src/utils/substractionMNGenerator.js
+++ math_ui/src/utils/substractionMNGenerator.js
@@ -20,9 +20,9 @@ const generateFullArr = () => {
 const filterRestrictions = (arr, restrictions) => {
   if (restrictions.includes(filters.SUBTRAHEND_LESSTHAN_10)) {
     arr = arr.filter(item => {
-      if (item[0][0] == "x") {
+      if (item[0][0] === "x") {
         return true;
-      } else if (item[0][1] == "x") {
+      } else if (item[0][1] === "x") {
         return item[0][2] < 10;
       } else {
         return item[0][1] < 10;
