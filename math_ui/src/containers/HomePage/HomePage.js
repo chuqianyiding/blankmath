2535aeac9e5d1e818880220a1407b996474377c8
Zoe Li <yanyanl@zillowgroup.com>
2019-03-28 16:38:25 -0700

    add little homepage

--- math_ui/src/containers/HomePage/HomePage.js
+++ math_ui/src/containers/HomePage/HomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./HomePage.scss";
+import bee from "../../images/bee-705412.svg";
 
 class HomePage extends React.Component {
   render() {
     return (
       <React.Fragment>
         <div className="header">Blankmath</div>
+        <div className="image-section">
+          <img src={bee} alt="bee" />
+        </div>
       </React.Fragment>
     );
   }
