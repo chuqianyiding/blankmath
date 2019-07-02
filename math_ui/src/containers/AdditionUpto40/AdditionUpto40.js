cee7c379cf60a63dd1145fe530983c9d7a0c0928
Yanyan Li <yyrobot@gmail.com>
2019-07-01 19:04:22 -0700

    add upto40 link

---
+++ math_ui/src/containers/AdditionUpto40/AdditionUpto40.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AdditionUpto40Page extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="mt-4">
+          <h3> Addition -- Numbers up to 40</h3>
+        </div>
+      </>
+    );
+  }
+}
+
+export default AdditionUpto40Page;
