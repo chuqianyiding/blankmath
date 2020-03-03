d295368de05cd6166c32599f48a1598804cada74
Yanyan Li <yyrobot@gmail.com>
2020-03-02 22:23:46 -0800

    add header click

--- math_ui/src/components/Header/Header.js
+++ math_ui/src/components/Header/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Header.scss";
 
-const Header = () => {
+const Header = ({ onClickLogo }) => {
   return (
     <div className="bm-header d-flex flex-md-row flex-column mb-3">
-      <div className="bm-header-title ">Blankmath</div>
+      <div className="bm-header-title" onClick={onClickLogo}>
+        Blankmath
+      </div>
       <div className="bm-header-subtitle ml-md-3 mt-2 align-self-md-center">
         Unlimited FREE math worksheets
       </div>
     </div>
   );
+};
+
+Header.propTypes = {
+  onClickLogo: PropTypes.func
 };
 
 export default Header;
--- math_ui/src/containers/App/App.js
+++ math_ui/src/containers/App/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import PropTypes from "prop-types";
+import { Switch, Route, withRouter } from "react-router-dom";
 import { Container } from "reactstrap";
 import SubtractionUpto20Page from "../SubtractionUpto20";
 import SubtractionMNUpto20 from "../SubtractionMissingNumberUpto20";
@@ -14,42 +15,51 @@ import MultiplicationMNUpto10 from "../MultiplicationMissingNumberUpto10";
 import Header from "../../components/Header";
 import "./App.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/add_upto20" component={AdditionUpto20Page} />
-          <Route
-            exact
-            path="/subtract_upto20"
-            component={SubtractionUpto20Page}
-          />
-          <Route
-            exact
-            path="/subtract_mn_upto20"
-            component={SubtractionMNUpto20}
-          />
-          <Route exact path="/add_mn_upto20" component={AdditionMNUpto20} />
-          <Route
-            exact
-            path="/greater_than_less_than"
-            component={GreatThanLessThanPage}
-          />
-          <Route path="/add_upto40" component={AdditionUpto40Page} />
-          <Route path="/subtract_upto40" component={SubtractionUpto40Page} />
-          <Route path="/add_three_numbers" component={AddThreeNumbersPage} />
-          <Route
-            exact
-            path="/multiply_mn_upto10"
-            component={MultiplicationMNUpto10}
-          />
-        </Switch>
-      </Container>
-    );
-  }
-}
+const App = ({ history }) => {
+  const handleClickLogo = () => {
+    history.push("/");
+  };
 
-export default App;
+  return (
+    <Container>
+      <Header onClickLogo={handleClickLogo} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/add_upto20" component={AdditionUpto20Page} />
+        <Route
+          exact
+          path="/subtract_upto20"
+          component={SubtractionUpto20Page}
+        />
+        <Route
+          exact
+          path="/subtract_mn_upto20"
+          component={SubtractionMNUpto20}
+        />
+        <Route exact path="/add_mn_upto20" component={AdditionMNUpto20} />
+        <Route
+          exact
+          path="/greater_than_less_than"
+          component={GreatThanLessThanPage}
+        />
+        <Route path="/add_upto40" component={AdditionUpto40Page} />
+        <Route path="/subtract_upto40" component={SubtractionUpto40Page} />
+        <Route path="/add_three_numbers" component={AddThreeNumbersPage} />
+        <Route
+          exact
+          path="/multiply_mn_upto10"
+          component={MultiplicationMNUpto10}
+        />
+      </Switch>
+    </Container>
+  );
+};
+
+App.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+    back: PropTypes.func
+  })
+};
+
+export default withRouter(App);
