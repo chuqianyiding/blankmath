515f9e319c78264ac9e8c971bf98f862b6c7cd0c
Yanyan Li <yyrobot@gmail.com>
2020-03-20 16:12:19 -0700

    move cardData to another file

---
+++ math_ui/src/constants/cardData.js
@@ -0,0 +1,68 @@
+export const cardData = [
+  {
+    cardKey: "additionCard",
+    sign: ["plus"],
+    title: "Addition",
+    subtitle: ["12 + 9 = ?", "2 + 8 = ?"]
+  },
+  {
+    cardKey: "mnAdditionCard",
+    sign: ["plus"],
+    title: "Missing Number",
+    subtitle: ["7 + ? = 15", "? + 3 = 12"]
+  },
+  {
+    cardKey: "tnpAdditionCard",
+    sign: ["plus"],
+    title: "Three Number",
+    subtitle: ["7 + 8 + 12 = ?", "2 + 3 + 4 = ?"]
+  },
+  {
+    cardKey: "minusCard",
+    sign: ["minus"],
+    title: "Minus",
+    subtitle: ["12 - 9 = ?", "8 - 2 = ?"]
+  },
+  {
+    cardKey: "mnMinusCard",
+    sign: ["minus"],
+    title: "Missing Number",
+    subtitle: ["7 - ? = 5", "? - 3 = 12"]
+  },
+  {
+    cardKey: "tnpAdditionMinusCard",
+    sign: ["plus", "minus"],
+    title: "Three Number",
+    subtitle: ["17 - 8 + 7 = ?", "8 + 3 - 4 = ?"]
+  },
+  {
+    cardKey: "timesCard",
+    sign: ["times"],
+    title: "Multiplication",
+    subtitle: ["8 * 9 = ?", "2 * 3 = ?"]
+  },
+  {
+    cardKey: "mnTimesCard",
+    sign: ["times"],
+    title: "Missing Number",
+    subtitle: ["8 * ? = 72", "2 * ? = 6"]
+  },
+  {
+    cardKey: "comparisionCard",
+    sign: ["greaterThan", "lessThan"],
+    title: "Comparision",
+    subtitle: ["7 > 8 or 7 < 8 ?", "12 > 2 or 12 < 2 ?"]
+  },
+  {
+    cardKey: "divisionCard",
+    sign: ["divide"],
+    title: "Division",
+    subtitle: ["8 / 2 = ?", "12 / 4 = ?"]
+  },
+  {
+    cardKey: "mnDivisionCard",
+    sign: ["divide"],
+    title: "Missing Number",
+    subtitle: ["8 / ? = 2", "12 / ? = 3"]
+  }
+];
--- math_ui/src/containers/HomePage/HomePage.js
+++ math_ui/src/containers/HomePage/HomePage.js
@@ -1,88 +1,9 @@
 import React from "react";
 import "./HomePage.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlus,
-  faMinus,
-  faGreaterThanEqual,
-  faTimes,
-  faDivide
-} from "@fortawesome/free-solid-svg-icons";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Link from "@material-ui/core/Link";
-import { Link as RouterLink } from "react-router-dom";
 import bee from "../../img/bee-705412.svg";
 import Card from "../../components/Card";
+import { cardData } from "../../constants/cardData";
 
-const cardData = [
-  {
-    cardKey: "additionCard",
-    sign: ["plus"],
-    title: "Addition",
-    subtitle: ["12 + 9 = ?", "2 + 8 = ?"]
-  },
-  {
-    cardKey: "mnAdditionCard",
-    sign: ["plus"],
-    title: "Missing Number",
-    subtitle: ["7 + ? = 15", "? + 3 = 12"]
-  },
-  {
-    cardKey: "tnpAdditionCard",
-    sign: ["plus"],
-    title: "Three Number",
-    subtitle: ["7 + 8 + 12 = ?", "2 + 3 + 4 = ?"]
-  },
-  {
-    cardKey: "minusCard",
-    sign: ["minus"],
-    title: "Minus",
-    subtitle: ["12 - 9 = ?", "8 - 2 = ?"]
-  },
-  {
-    cardKey: "mnMinusCard",
-    sign: ["minus"],
-    title: "Missing Number",
-    subtitle: ["7 - ? = 5", "? - 3 = 12"]
-  },
-  {
-    cardKey: "tnpAdditionMinusCard",
-    sign: ["plus", "minus"],
-    title: "Three Number",
-    subtitle: ["17 - 8 + 7 = ?", "8 + 3 - 4 = ?"]
-  },
-  {
-    cardKey: "timesCard",
-    sign: ["times"],
-    title: "Multiplication",
-    subtitle: ["8 * 9 = ?", "2 * 3 = ?"]
-  },
-  {
-    cardKey: "mnTimesCard",
-    sign: ["times"],
-    title: "Missing Number",
-    subtitle: ["8 * ? = 72", "2 * ? = 6"]
-  },
-  {
-    cardKey: "comparisionCard",
-    sign: ["greaterThan", "lessThan"],
-    title: "Comparision",
-    subtitle: ["7 > 8 or 7 < 8 ?", "12 > 2 or 12 < 2 ?"]
-  },
-  {
-    cardKey: "divisionCard",
-    sign: ["divide"],
-    title: "Division",
-    subtitle: ["8 / 2 = ?", "12 / 4 = ?"]
-  },
-  {
-    cardKey: "mnDivisionCard",
-    sign: ["divide"],
-    title: "Missing Number",
-    subtitle: ["8 / ? = 2", "12 / ? = 3"]
-  }
-];
 class HomePage extends React.Component {
   render() {
     return (
