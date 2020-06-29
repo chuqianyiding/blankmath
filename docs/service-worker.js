f477daa894059b7f0221e6e1541f3ba16901765c
Travis CI <d@domenic.me>
2020-06-29 14:44:42 +0000

    Deploy to GitHub Pages: d08fc08fb40c0bc7d070ef3d4a14fa748376227b

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef57f2fcd096c02bf707eefd18e4d90c.js"
+  "/precache-manifest.9721e808642664be8fa533e771a333c3.js"
 );
 
 workbox.clientsClaim();
