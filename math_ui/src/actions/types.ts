7ac77eba63f468bad5451c8679ee7cd8175ddf91
Yanyan Li <yyrobot@gmail.com>
2020-07-02 13:03:31 -0700

    remove comment code

--- math_ui/src/actions/types.ts
+++ math_ui/src/actions/types.ts
@@ -1,14 +1,5 @@
 import { Action } from "redux";
 
-// export function typedAction<T extends string>(type: T): { type: T };
-// export function typedAction<T extends string, P extends any>(
-//   type: T,
-//   payload: P
-// ): { type: T; payload: P };
-// export function typedAction(type: string, payload?: any) {
-//   return { type, payload };
-// }
-
 export interface typedAction extends Action {
   type: string;
   payload?: any;
