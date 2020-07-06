a4653b4b6d6e6f4cccf2aeae865efe82acb8fb03
Yanyan Li <yyrobot@gmail.com>
2020-07-05 19:43:13 -0700

    update container

---
+++ math_ui/src/containers/AddThreeNumbers/AddThreeNumbers.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { connect } from "react-redux";
+import ProblemNumber from "../../components/ProblemNumber";
+import {
+  updateProblemNumber,
+  updateNumberOfDigits,
+} from "../../actions/addThreeNumbersActions";
+import { typedAction } from "../../actions/types";
+import { ApplicationState } from "../../reducers";
+import NumberOfDigits from "../../components/NumberOfDigits";
+import Button from "@material-ui/core/Button";
+import { generateThreeNumbers } from "../../utils/addThreeNumbersGenerator";
+import axios from "axios";
+import config from "../../config.json";
+
+interface AddThreeNumbersPageProps {
+  updateProblemNumber: (value: string) => typedAction;
+  updateNumberOfDigits: (value: string) => typedAction;
+  problemValue: string;
+  digitValue: string;
+}
+
+const AddThreeNumbersPage:React.FC<AddThreeNumbersPageProps> =({
+  updateProblemNumber,
+  updateNumberOfDigits,
+  problemValue,
+  digitValue
+}:AddThreeNumbersPageProps) => {
+  const handleProblemNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateProblemNumber(event.target.value);
+  };
+
+  const handleDigitValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateNumberOfDigits(event.target.value);
+  };
+
+  const handleClickCreate = () => {
+    const problems = generateThreeNumbers(
+      digitValue,
+      parseInt(problemValue, 10)
+    );
+
+    axios
+      .post(config.PDFGeneratorEndpoint, {
+        equations: problems,
+        template: "3num",
+      })
+      .then((resp) => {
+        window.open(resp.data);
+      });
+  };
+
+  return (
+    <>
+      <div className="mt-5">
+        <ProblemNumber
+          problemValue={problemValue}
+          onProblemNumberChange={handleProblemNumberChange}
+        />
+      </div>
+
+      <div className="mt-4">
+        <NumberOfDigits
+          digitValue={digitValue}
+          onDigitChange={handleDigitValueChange}
+        />
+      </div>
+
+      <div className="mt-4">
+        {" "}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleClickCreate}
+        >
+          Create
+        </Button>
+      </div>
+    </>
+  );
+}
+
+
+const mapStateToProps = (state:ApplicationState) => ({
+  problemValue: state.addThreeNumbersData.problemNumber,
+  digitValue: state.addThreeNumbersData.digitNumber,
+});
+
+const mapDispatchToProps = {
+  updateProblemNumber,
+  updateNumberOfDigits,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddThreeNumbersPage);
